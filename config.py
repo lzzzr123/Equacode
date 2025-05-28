8f4e925171f7bfa7c5255ec3e54faa279b0dfdad
lzzzr123 <liangzhen741@gmail.com>
2025-05-28 19:51:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+base_url="https://api.kksj.org/v1"
+
+openai_key="sk-D8HZrFOQGwA0GHn2kdym30T8OBjQBdbIXkZF6nwXMU04KyJp"
+
+target_model="gpt-3.5-turbo"
+
+assessmodel="llama3.1:70b"
+
+
+assess_base_url="http://10.16.23.106:11434/v1"
